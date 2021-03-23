cfd324336507ae5a8294b0cbdd9fd150f97762d5
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-23 14:44:21 -0400

    Create index.php

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Cam-Lock</title>
+    </head>
+
+    <body>
+        <h1>Biometric Lock</h1>
+        <h2>Camera Feed</h2>
+        <br>
+
+        <img style="-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 25%);" 
+        src="http://192.168.2.38:8081/" width="387" height="290">
+
+    </body>
+</html>
