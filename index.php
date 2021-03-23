443fe7ce54595b8aa7404b8cb3afec7af2bfa0ce
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-23 16:47:39 -0400

    Update index.php

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
         <h2>Camera Feed</h2>
         <br>
 
-        <img src="http://192.168.2.38:8081/" alt="Camera is down :(">
+        <img src="http://192.168.2.38:8081/" alt="Camera is not here :(">
 
     </body>
 </html>
