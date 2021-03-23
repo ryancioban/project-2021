7d3f03d6ec463aec081b5dbcdadc614d2d2b0a27
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-23 14:51:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <br>
 
         <img style="-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 25%);" 
-        src="http://192.168.2.38:8081/" width="387" height="290">
+        src="http://192.168.2.38:8081/" >
 
     </body>
 </html>
