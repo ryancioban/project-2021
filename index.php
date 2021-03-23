23551c79d20973787da6cf93c461f53b84e190b6
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-23 14:55:13 -0400

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
         <br>
 
         <img style="-webkit-user-select: none;margin: auto;background-color: hsl(0, 0%, 25%);" 
-        src="http://192.168.2.38:8081/" >
+        src="http://192.168.2.38:8081/" alt="Camera is down :(">
 
     </body>
 </html>
