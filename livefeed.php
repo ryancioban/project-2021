9de6b3f74de40f423dae6b577916ad450f432df5
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-30 16:26:28 -0400

    Update livefeed.php

--- livefeed.php
+++ livefeed.php
@@ -1,1 +1,15 @@
-Hello Ryan
+<br>
+<div>Hello humans!</div>
+<br>
+
+<?php
+$output = shell_exec('gpio read 2');
+
+if (($output = `gpio read 2`) == 0){
+                echo "<br>Switch is LOW!<br>";
+            }
+
+else if (($output = `gpio read 2`) == 1){
+                echo "<br>Switch is HIGH!<br>";
+            }
+?>
