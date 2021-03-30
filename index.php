255ca6688c1420e2fab921dd3ba1b7bf8265edf6
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-30 14:42:34 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,14 +1,7 @@
 <!DOCTYPE html>
-<?php
-    //Uncomment these lines for auto-refresh
-    //$page = $_SERVER['PHP_SELF'];
-    //$sec = "10";
-?>
 <html>
     <head>
         <title>Cam-Lock</title>
-        <!--Auto-refresh page-->
-        <meta http-equiv="refresh" content="<?php echo $sec?>;URL='<?php echo $page?>'">
     </head>
 
     <body>
@@ -16,7 +9,7 @@
         <h2>Camera Feed</h2>
         <br>
 
-        <img src="http://192.168.2.38:8081/" alt="Camera is down :(">
+        <img src="http://pizero:8081/" alt="Camera is down :(">
 
     </body>
 </html>
