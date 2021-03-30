a008b1e29712ae819092ad279032e3b3597ec8cf
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-30 17:16:17 -0400

    Update index.php

--- index.php
+++ index.php
@@ -26,5 +26,20 @@
         <div id="liveData">
             <p>Loading Data...</p>
         </div>
+
+        <br>
+        <div>Testing</div>
+        <br>
+        <?php
+            $output = shell_exec('ssh pi@192.168.2.38 gpio read 2');
+
+            if ($output == 0){
+                echo "<br>Switch is LOW!<br>";
+                }
+
+            else if ($output == 1){
+                echo "<br>Switch is HIGH!<br>";
+                }
+        ?>
     </body>
 </html>
