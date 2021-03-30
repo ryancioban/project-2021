b78bbf1d82b858416ebff40e174b1644c8135cd3
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-03-30 15:03:48 -0400

    Update index.php

--- index.php
+++ index.php
@@ -4,14 +4,24 @@
         <title>Cam-Lock</title>
     </head>
 
-    <body>
+    <body onload= "getDate();">
         <h1>Biometric Lock</h1>
         <h2>Camera Feed</h2>
         <br>
 
         <img src="http://pizero:8081/" alt="Camera is down :(">
         <br>
-        <p>The time is <%= DateTime.Now %></p>
+        <p>The time is : <span id="date"></span>.</p>
+
+
+        <script type="text/javascript" language="javascript">
+
+        function getDate() {
+            alert(Date());
+            document.getElementById('date').innerText = Date().toString();
+        }
+
+    </script>
 
     </body>
 </html>
