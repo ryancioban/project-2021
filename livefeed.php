faf4e9407ce6ec2c4c29f30023688fbd7851ba10
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-18 12:56:18 -0400

    IP update

--- livefeed.php
+++ livefeed.php
@@ -2,10 +2,10 @@
 $output = shell_exec('ssh pi@pizero gpio read 2');
 
 if ($output == 0){
-    echo "<br>Switch is LOW!<br>";
+    echo "<br>Door Status: LOCKED<br>";
 }
 
 else if ($output == 1){
-    echo "<br>Switch is HIGH!<br>";
+    echo "<br>Door Status: UNLOCKED<br>";
 }
 ?>
