9ffb24d73a29f6d55bd020b96eab03ab4c9a2b92
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 03:55:20 -0400

    Update livefeed.php

--- livefeed.php
+++ livefeed.php
@@ -4,31 +4,25 @@ $log = shell_exec('tail -n 15 /home/pi/test1.txt');
 //echo date ('H:i:s');
 //$buf = 1;
 
-if ($output == 0) && ($buf == false){
+if ($output == 0){
     echo "<br>Door Status: LOCKED<br>";
     $buf = 0;
-}
-
-else if ($output == 0) && ($buf == true){
-    echo "<br>Door Status: LOCKED<br>";
-    echo "<br>Buffer set!<br>";
-    $buf = 1;
 }
 
 else if ($output == 1){
     echo "<br>Door Status: UNLOCKED<br>";
     //$buf++;
 
-    if ($buf == 1) {
+    if ($buf == 0) {
         echo "<br>Door Status: UNLOCKED bruh<br>";
         $comm = shell_exec('echo "Unlocked via Fingerprint @ `date` <br>" >> /home/pi/test1.txt');
         echo "<div>$comm</div>";
-        $buf = 0;
+        $buf = 1;
     }
 
-   else {
-        echo "<br>Door Status: UNLOCKED<br>";
-    }
+   //else {
+    //    echo "<br>Door Status: UNLOCKED<br>";
+   // }
 }
 
 //echo "<div>$comm</div>";
