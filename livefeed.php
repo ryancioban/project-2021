8b6938c849d0ae706baea7f7be68cc11c13e8c66
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 02:11:23 -0400

    Update livefeed.php

--- livefeed.php
+++ livefeed.php
@@ -9,8 +9,8 @@ if ($output == 0){
 else if ($output == 1){
     echo "<br>Door Status: UNLOCKED<br>";
     $comm = shell_exec('echo "Unlocked via Fingerprint @ `date` <br>" >> /home/pi/test1.txt');
-    echo "<div>$comm</div>";
 }
 
+echo "<div>$comm</div>";
 echo "<br><div>$log</div><br>";
 ?>
