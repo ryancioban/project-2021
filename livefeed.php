f46fb73f02c6c3a5be196d9359828ae36f49bd89
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 03:54:11 -0400

    Update livefeed.php

--- livefeed.php
+++ livefeed.php
@@ -2,15 +2,21 @@
 $output = shell_exec('ssh pi@192.168.2.38 gpio read 2');
 $log = shell_exec('tail -n 15 /home/pi/test1.txt');
 //echo date ('H:i:s');
-$buf = 1;
+//$buf = 1;
 
-if ($output == 0){
+if ($output == 0) && ($buf == false){
     echo "<br>Door Status: LOCKED<br>";
+    $buf = 0;
+}
+
+else if ($output == 0) && ($buf == true){
+    echo "<br>Door Status: LOCKED<br>";
+    echo "<br>Buffer set!<br>";
     $buf = 1;
 }
 
 else if ($output == 1){
-    //echo "<br>Door Status: UNLOCKED<br>";
+    echo "<br>Door Status: UNLOCKED<br>";
     //$buf++;
 
     if ($buf == 1) {
