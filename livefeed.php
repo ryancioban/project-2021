3dd675ce204841051d392c018019cee834c034a5
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 01:39:37 -0400

    Update livefeed.php

--- livefeed.php
+++ livefeed.php
@@ -1,11 +1,15 @@
 <?php
 $output = shell_exec('ssh pi@192.168.2.38 gpio read 2');
+$log = shell_exec('tail -n 15 /home/pi/test1.txt')
 
 if ($output == 0){
     echo "<br>Door Status: LOCKED<br>";
+
 }
 
 else if ($output == 1){
     echo "<br>Door Status: UNLOCKED<br>";
 }
+
+echo "<br><div>$log</div><br>";
 ?>
