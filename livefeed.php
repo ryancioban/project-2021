3c7252c72cd95cf625c8c4f57769779169093e56
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 03:03:55 -0400

    Update livefeed.php

--- livefeed.php
+++ livefeed.php
@@ -2,21 +2,22 @@
 $output = shell_exec('ssh pi@192.168.2.38 gpio read 2');
 $log = shell_exec('tail -n 15 /home/pi/test1.txt');
 //echo date ('H:i:s');
-//$buf;
+$buf;
 
 if ($output == 0){
     echo "<br>Door Status: LOCKED<br>";
+    $buf = 1;
 }
 
 else if ($output == 1){
     echo "<br>Door Status: UNLOCKED<br>";
     //$buf++;
 
-    //if ($buf == 10) {
+    if ($buf == 1) {
         $comm = shell_exec('echo "Unlocked via Fingerprint @ `date` <br>" >> /home/pi/test1.txt');
         echo "<div>$comm</div>";
-        //$buf = 0;
-    //}
+        $buf = 0;
+    }
 }
 
 //echo "<div>$comm</div>";
