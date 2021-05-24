fed949f9f181565ecce0e96b76d29af62b5b0089
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 02:31:52 -0400

    test new changes

--- livefeed.php
+++ livefeed.php
@@ -1,6 +1,6 @@
 <?php
 $output = shell_exec('ssh pi@192.168.2.38 gpio read 2');
-$log = shell_exec('tail -n 15 /home/pi/test1.txt');
+//$log = shell_exec('tail -n 15 /home/pi/test1.txt');
 //echo date ('H:i:s');
 if ($output == 0){
     echo "<br>Door Status: LOCKED<br>";
@@ -8,10 +8,10 @@ if ($output == 0){
 
 else if ($output == 1){
     echo "<br>Door Status: UNLOCKED<br>";
-    $comm = shell_exec('echo "Unlocked via Fingerprint @ `date` <br>" >> /home/pi/test1.txt');
-    sleep(10);
+   // $comm = shell_exec('echo "Unlocked via Fingerprint @ `date` <br>" >> /home/pi/test1.txt');
+    //sleep(10);
 }
 
-echo "<div>$comm</div>";
-echo "<br><div>$log</div><br>";
+//echo "<div>$comm</div>";
+//echo "<br><div>$log</div><br>";
 ?>
---
+++ livefeed2.php
@@ -0,0 +1,17 @@
+<?php
+$output = shell_exec('ssh pi@192.168.2.38 gpio read 2');
+$log = shell_exec('tail -n 15 /home/pi/test1.txt');
+//echo date ('H:i:s');
+//if ($output == 0){
+   //echo "<br>Door Status: LOCKED<br>";
+//}
+
+if ($output == 1){
+    //echo "<br>Door Status: UNLOCKED<br>";
+    $comm = shell_exec('echo "Unlocked via Fingerprint @ `date` <br>" >> /home/pi/test1.txt');
+    sleep(10);
+}
+
+echo "<div>$comm</div>";
+echo "<br><div>$log</div><br>";
+?>
