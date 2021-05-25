697b298f3c205dfbe3146860aebc226ca1b7a008
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 23:51:28 -0400

    Update OpenDoor.php

--- OpenDoor.php
+++ OpenDoor.php
@@ -5,13 +5,14 @@
     </head>
     <body>
         <?php
-            //Output logic high from Pi Zero pin.
+            //Output logic HIGH from Pi Zero pin.
             shell_exec('ssh pi@192.168.2.38 gpio write 3 1');
             //Timestamp to logs
             shell_exec('echo "Unlocked via Web @ `date` <br>" >> /home/pi/test1.txt');
             echo "<br>Opening the lock...<br>";
             //Buffer for propagation
             sleep(5);
+            //Reset pin to LOW.
             shell_exec('ssh pi@192.168.2.38 gpio write 3 0');
         ?>
         <br>
