f1563fc66211d9d9b7b5d14929c32b9e110f10a3
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 23:32:10 -0400

    Update OpenTheDoor.js

--- OpenTheDoor.js
+++ OpenTheDoor.js
@@ -2,7 +2,7 @@ function control(){
     shell_exec('ssh pi@192.168.2.38 gpio write 3 1');
     shell_exec('echo "Unlocked via Web @ `date` <br>" >> /home/pi/test1.txt');
 
-    setTimeout(function(){
-        shell_exec('ssh pi@192.168.2.38 gpio write 3 0');
-    }, 4000);
+    //setTimeout(function(){
+       // shell_exec('ssh pi@192.168.2.38 gpio write 3 0');
+   // }, 4000);
 }
