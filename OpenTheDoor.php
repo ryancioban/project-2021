7ffa88b502429d55942e77f9b6cef9d436622c0b
ryancioban <70660684+ryancioban@users.noreply.github.com>
2021-05-24 23:44:05 -0400

    Update OpenTheDoor.php

--- OpenTheDoor.php
+++ OpenTheDoor.php
@@ -8,6 +8,8 @@
             shell_exec('ssh pi@192.168.2.38 gpio write 3 1');
             shell_exec('echo "Unlocked via Web @ `date` <br>" >> /home/pi/test1.txt');
             echo "<br>Opening the lock...<br>";
+            sleep(5);
+            shell_exec('ssh pi@192.168.2.38 gpio write 3 0');
         ?>
         <div>Go back to<a href="index.php">main</a> page.</div>
     </body>
